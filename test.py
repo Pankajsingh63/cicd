f1e4d938319ee4dbbb0299e767d0bf2cf1f3ec18
Pankajsingh63 <34594483+Pankajsingh63@users.noreply.github.com>
2019-08-18 18:51:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-#!/bin/env/python 
-print("Hello World")
+print ("Hello This is a simple python script to find out the prime number")
+num1 = int(input("Please enter the number here:"))
+if (num1%2) == 0:
+    then: print("num1 is even")
+else: print("num1 is odd")
